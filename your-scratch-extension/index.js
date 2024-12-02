d9e6bbfc1b7e9782e3c649a4a9010ed70d48f2cf
Escuela de Oficios Electrosonoros <gurrupibaby@gmail.com>
2024-12-02 10:52:06 +0100

    Update index.js

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -6,6 +6,10 @@ class Scratch3YourExtension {
 
     constructor (runtime) {
         // put any setup for your extension here
+        import('syllable')
+            .then((syllableModule) => {
+                this.syllable = syllableModule.syllable;
+            });
     }
 
     /**
@@ -14,18 +18,18 @@ class Scratch3YourExtension {
     getInfo () {
         return {
             // unique ID for your extension
-            id: 'yourScratchExtension',
+            id: 'sonotronicaScratchExtension',
 
             // name that will be displayed in the Scratch UI
-            name: 'Demo',
+            name: 'Sonotronica',
 
             // colours to use for your extension blocks
             color1: '#000099',
             color2: '#660066',
 
             // icons to display
-            blockIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
-            menuIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
+            //blockIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
+            //menuIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
 
             // your Scratch blocks
             blocks: [
@@ -81,6 +85,60 @@ class Scratch3YourExtension {
                             type: ArgumentType.STRING
                         }
                     }
+                },
+                {
+                    // name of the function where your block code lives
+                    opcode: 'mySecondBlock',
+
+                    // type of block - choose from:
+                    blockType: BlockType.REPORTER,
+
+                    // label to display on the block
+                    text: 'Title for ISBN book [BOOK_NUMBER]',
+
+                    // true if this block should end a stack
+                    terminal: false,
+
+                    // where this block should be available for code - choose from:
+                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
+
+                    // arguments used in the block
+                    arguments: {
+                        BOOK_NUMBER: {
+                            // default value before the user sets something
+                            defaultValue: 1718500564,
+
+                            // type/shape of the parameter - choose from:
+                            type: ArgumentType.NUMBER
+                        }
+                    }
+                },
+                {
+                    // name of the function where your block code lives
+                    opcode: 'myThirdBlock',
+
+                    // type of block - choose from:
+                    blockType: BlockType.REPORTER,
+
+                    // label to display on the block
+                    text: 'Syllables in [MY_TEXT]',
+
+                    // true if this block should end a stack
+                    terminal: false,
+
+                    // where this block should be available for code - choose from:
+                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
+
+                    // arguments used in the block
+                    arguments: {
+                        MY_TEXT: {
+                            // default value before the user sets something
+                            defaultValue: 'Hello World',
+
+                            // type/shape of the parameter - choose from:
+                            type: ArgumentType.STRING
+                        }
+                    }
                 }
             ]
         };
@@ -95,6 +153,35 @@ class Scratch3YourExtension {
         // example implementation to return a string
         return MY_STRING + ' : doubled would be ' + (MY_NUMBER * 2);
     }
+
+    /**
+     * implementation of the block with the opcode that matches this name
+     *  this will be called when the block is used
+     */
+    mySecondBlock ({ BOOK_NUMBER }) {
+        // example fetching title from ISBN API
+        return fetch('https://openlibrary.org/isbn/' + BOOK_NUMBER + '.json')
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                else {
+                    return {title : 'Unknown'};
+                }
+            })
+            .then((bookinfo) => {
+                return bookinfo.title;
+            });
+    }
+
+        /**
+     * implementation of the block with the opcode that matches this name
+     *  this will be called when the block is used
+     */
+        myThirdBlock ({ MY_TEXT }) {
+            // example of syllable npm package
+            return this.syllable(MY_TEXT);
+        }
 }
 
 module.exports = Scratch3YourExtension;
